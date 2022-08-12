af142eb23a0b5de7658e163e15ac3dfaf4c03cdf
lala_zq <2767329009@qq.com>
2022-08-12 11:46:27 +0800

    feat(Add)

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+/**
+ * @author lala_zq
+ * @createTime 2022/8/12
+ */
+public class Main{
+    public static void main(String[] args)
+    {
+        double a=1;
+        double b=2;
+        double c;
+        numberAdd n=new numberAdd();
+        c=n.add(a,b);
+        System.out.println(c);
+    }
+}
---
+++ src/main/java/numberAdd.java
@@ -0,0 +1,10 @@
+/**
+ * @author lala_zq
+ * @createTime 2022/8/12
+ */
+public class numberAdd {
+    public double add(double a, double b) {
+        return a + b;
+    }
+}
+
